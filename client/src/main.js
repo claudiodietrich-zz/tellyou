85255cd0744610d9f645b8add9b82625397f6e5f
Claudio Dietrich <crdietrichjr@gmail.com>
2019-04-26 23:50:59 -0300

    add axios and vue-session

--- client/src/main.js
+++ client/src/main.js
@@ -6,16 +6,31 @@ import Buefy from 'buefy'
 import Vuelidate from 'vuelidate'
 import VueI18n from 'vue-i18n'
 import messages from '@/i18n/i18n'
+import axios from 'axios'
+import VueSession from 'vue-session'
 
 Vue.use(Buefy)
 Vue.use(Vuelidate)
 Vue.use(VueI18n)
+Vue.use(VueSession, { persist: true })
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = 'http://localhost:3000/api'
 
 const i18n = new VueI18n({
   locale: 'ptBR', // set locale
   messages // set locale messages
+})
+
+router.beforeEach((to, from, next) => {
+  const sessionExists = router.app.$session.exists()
+  const requiresSession = to.matched.some(route => route.meta.requiresSession)
+
+  if (requiresSession && !sessionExists) {
+    next({ name: 'home' })
+  }
+
+  next()
 })
 
 new Vue({
